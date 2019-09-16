bafec5368514fc0d131d3df05ea2568b85413718
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-09-16 15:02:32 -0300

    modificações na estrutura e estilizações

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
+
+
 <template>
   <div class="corpo">
-    <h3 class="centralizado">{{ titulo }}</h3>
+    <h1 class="centralizado">{{ titulo }}</h1>
+
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <div class="painel">
+          <h2 class="painel-titulo"> {{ foto.titulo }}</h2>
+            <div class="painel-conteudo">
+              <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+            </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -11,50 +19,78 @@
 
 <script>
 export default {
-
   data() {
-
     return {
-      'titulo' : 'PIC',
-      'fotos' : [
+      titulo: "PIC",
+      fotos: [
         {
-        url : 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-        titulo: 'Cachorro'
+          url:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          titulo: "Cachorro"
         },
         {
-        url : 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-        titulo: 'Cachorrão'
+          url:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          titulo: "Cachorrão"
         }
       ]
-
-
-    }
+    };
   },
 
-  created(){
-    this.$http.get('http://localhost:3000/v1/fotos')
-    .then(res => res.json())
-    .then(fotos => this.fotos = fotos, err => console.log(err));
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
   }
-}
+};
 </script>
 
-<style>
-  .corpo{
-    font-family: Helvetica, sans-serif;
-    width: 96%;
-    margin: 0 auto;
-  }
 
-  .centralizado{
+<style>
+
+  .titulo {
     text-align: center;
   }
 
-  .lista-fotos{
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
     list-style: none;
   }
 
-  .lista-fotos .lista-fotos-item{
+  .lista-fotos .lista-fotos-item {
     display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  /* estilo do painel */
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
   }
 </style>
